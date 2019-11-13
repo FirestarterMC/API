38c765afec7df2a9046a151d78a5685b296f3e03
Mykyta <nkomarn@hotmail.com>
2019-11-12 18:21:18 -0800

    🔑 Start work on Discord auth

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
-const express = require('express')
-const app = express()
-const body_parser = require('body-parser')
-const mongoose = require('mongoose')
 const dotenv = require('dotenv')
+dotenv.config()
+
+const express = require('express')
+const session = require('express-session')
+const app = express()
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+/*const Sequelize = require('sequelize')
+const sequelize = new Sequelize(process.env.DB, process.env.DB_USERNAME,
+    process.env.DB_PASSWORD, {
+        host: process.env.DB_HOST,
+        dialect: 'mysql'
+    });
+*/
 
 const port = process.env.PORT || 8080
 const router = express.Router()
 
 const Player = require('./models/player')
 
+app.use(session({secret: process.env.SECRET}))
+app.use(bodyParser.json())
 app.use('/api', router)
-app.use(body_parser.urlencoded({extended: true}))
-app.use(body_parser.json())
 
 router.use((req, res, next) => {
     console.log(`🎺 Request from ${req.hostname}.`)
@@ -36,8 +46,37 @@ router.route('/player/:uuid').get((req, res) => {
     })
 })
 
-dotenv.config()
-mongoose.connect(process.env.MONGO_URI)
+// Authorization endpoint
+router.route('/authorize').get((req, res) => {
+    const body = req.body
+    if (Object.keys(body).length < 1) {
+        res.status(400).json({
+            "message": "No body."
+        })
+        return
+    }
+
+    if (body['uuid'] === undefined) {
+        res.status(400).json({
+            "message": "No UUID."
+        })
+        return
+    }
+
+    // TODO check if UUID is valid
+    const uuid = 'facb6ce6-338c-4bad-981d-e3b24b3453f6'
+    res.status(200).redirect(`https://discordapp.com/api/oauth2/authorize?client_id=619754624257228800&redirect_uri=http%3A%2F%2Fdd2f8c24.ngrok.io%2Fauthorize&response_type=code&scope=identify`)
+})
+
+router.route('/authorize').post((req, res) => {
+    console.log(req.query.uuid)
+    console.log(req.body)
+})
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true, useUnifiedTopology: true
+})
+
 app.listen(port, () => {
     console.log(`⚡ Listening on port ${port}.`)
 })
