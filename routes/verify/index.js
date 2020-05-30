2bf4a637e3e3806d26cad553ed73f0fa101ec9a6
Mykyta <nkomarn@hotmail.com>
2020-05-30 13:37:55 -0700

    ✌ Clean up the API endpoint some more I think this actually was close to the best way to do it- I still have to add some more checks but this is pretty good so far.

--- routes/verify/index.js
+++ routes/verify/index.js
@@ -18,11 +18,12 @@ router.use(session({
 router.get('/auth', (req, res) => {
     let code = req.query.code, token = req.session.token
 
-    if (code === undefined || token === undefined) {
-        return res.status(400).json({message: 'Bad Request.'})
+    if (!code || !token) {
+      return res.status(400).json({message: 'Bad Request.'})
     }
 
-    let linkDiscord = async () => {
+    // Fetch a Discord access token, the player's Discord id, and their Minecraft UUID
+    let getUserData = async () => {
       let accessToken = await axios.post('https://discord.com/api/v6/oauth2/token', qs.stringify({
         client_id: '619754624257228800',
         client_secret: process.env.CLIENT_SECRET,
@@ -47,45 +48,47 @@ router.get('/auth', (req, res) => {
       })
 
       let uuid = await client.hget('discord', token, (err, uuid) => {
-        if (uuid === null) {
-          throw 'Invalid token provided.'
-        } else {
-          return uuid
-        }
+        if (!uuid || err) throw err ? err : 'Invalid token provided.'
+        else return uuid
       })
-
-      let status = await Player.update({discord: discordId}, {
-        where: {uuid: uuid}
-      }).then(() => {
-        req.session.destroy()
-        client.hdel('discord', token)
-      }).then(() => {
-        return axios.put(`https://discordapp.com/api/guilds/609452308161363995/members/${discordId}`, {
-          'access_token': accessToken
-        }, {
-          headers: {
-            'Authorization': `Bot ${process.env.BOT_TOKEN}`,
-            'Content-Type': 'application/json'
-          }
-        })
-      }).then(response => {
-        return response.status
-      })
-
-      return status
+    
+      return [accessToken, discordId, uuid]
     }
 
-    linkDiscord().then(response => {
-      if (response === 204) {
-        return res.status(200).send('Your Discord account has been linked. You may now close this window.')
-      } else {
-        return res.status(200).send('You\'ve been added to the Discord server and your account has been linked. ' +
-          'You may now close this window.')
-      }
-    }).catch(err => {
-      console.error(err);
-      return res.status(500).send('Failed to verify your Discord account.')
-    })
+    getUserData()
+      .then((data) => {
+        let [accessToken, discordId, uuid] = data
+
+        Player.update({discord: discordId}, {
+          where: {uuid}
+        })
+        .then(() => {
+          req.session.destroy
+          return client.hdel('discord', token)
+        })
+        .then(() => {
+          return axios.put(`https://discordapp.com/api/guilds/609452308161363995/members/${discordId}`, {
+            'access_token': accessToken
+          }, {
+            headers: {
+              'Authorization': `Bot ${process.env.BOT_TOKEN}`,
+              'Content-Type': 'application/json'
+            }
+          })
+        })
+        .then((response) => {
+          if (response.status === 204) {
+            res.status(200).send('Your Discord account has been linked. You may now close this window.')
+          } else {
+            res.status(200).send('You\'ve been added to the Discord server and your account has been linked. ' +
+              'You may now close this window.')
+          }
+        })
+      })
+      .catch(err => {
+        console.error(err);
+        res.status(500).send('Failed to verify your Discord account.')
+      })
 });
 
 router.get('/:token', (req, res) => {
@@ -93,8 +96,8 @@ router.get('/:token', (req, res) => {
         if (err) {
             res.status(500).json({message: 'Internal Server Error.'})
         } else {
-            if (result === null) {
-                res.status(400).json({message: "Bad Request."})
+            if (!result) {
+              res.status(400).json({message: "Bad Request."})
             } else {
                 req.session.token = req.params.token;
                 res.redirect(process.env.OAUTH_REDIRECT)
